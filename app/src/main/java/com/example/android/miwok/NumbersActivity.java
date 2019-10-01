e8b9bdbfab5181223df1f755943643c01ed6cd45
sstopin <sstopin@gmail.com>
2019-09-30 19:31:56 -0700

    type:  New Features
    Added custom Word Class to contain the miwok and english words

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,11 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -27,19 +31,9 @@ public class NumbersActivity extends AppCompatActivity {
         numbers.add("nine");
         numbers.add("ten");
 
-        LinearLayout numbersLayout = (LinearLayout) findViewById(R.id.rootView);
-        TextView numbersTextView = new TextView(this);
-        numbersTextView.setText(numbers.get(0));
-        numbersLayout.addView(numbersTextView);
-
-        TextView numbersTextView1 = new TextView(this);
-        numbersTextView1.setText(numbers.get(1));
-        numbersLayout.addView(numbersTextView1);
-
-        TextView numbersTextView2 = new TextView(this);
-        numbersTextView2.setText(numbers.get(2));
-        numbersLayout.addView(numbersTextView2);
-
-
+        ArrayAdapter<String> itemsAdapter =
+            new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, numbers);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
     }
 }
